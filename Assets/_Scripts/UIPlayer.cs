20f6dfdc74719f04f94f844af382c1ee6d1ab6cc
0Red1 <rom1l.jeux@gmail.com>
2023-02-07 10:56:52 +0100

    Truc chiant playerHealthBar
    Truc chiant playerHealthBar tout est dit...

---
+++ Assets/_Scripts/UIPlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayer : MonoBehaviour
+{
+	#region Variables
+    public Slider playerHealthBar;
+    public Transform playerPosition;
+    #endregion
+
+    #region Properties
+    #endregion
+	
+	
+    #region Builts Methods
+    void Start()
+    {
+        
+    }
+	
+	
+    
+    void Update()
+    {
+        //playerPosition = 
+        //playerHealthBar = playerPosition;
+    }
+	#endregion
+
+    #region Custom Methods
+    #endregion
+}
