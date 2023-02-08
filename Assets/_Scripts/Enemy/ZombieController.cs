0d4ace60ec9286caa776215232965d18127aeb5f
Kurib3X <dubois.thomas062@gmail.com>
2023-02-08 14:46:41 +0100

    ZombieFini(prefab)

--- Assets/_Scripts/Enemy/ZombieController.cs
+++ Assets/_Scripts/Enemy/ZombieController.cs
@@ -9,13 +9,19 @@ public class ZombieController : MonoBehaviour
 	[SerializeField] private float attackRange;
 	[SerializeField] private float detectionRange;
 	[SerializeField] private float chaseRange;
+	[SerializeField] private int offsetRandomDestination;
+	[SerializeField] private float timeBetweenAttacks;
 
 	private NavMeshAgent _navAgent;
 	private GameObject _player;
-	private float _originY; // Lock Y pour le déplacement car bug j'ai pas trouvé
-	private GameObject _target;
 	private GameObject _attackDetect;
+	private GameObject _ground;
+
+	private GameObject _target;
 	private bool _attack = false;
+	private Vector2 _groundSize;
+	private bool _hasRandomDestination = false;
+	private bool _canAttack = false;
 	#endregion
 	
 	#region Properties
@@ -25,34 +31,47 @@ public class ZombieController : MonoBehaviour
 	void Start()
     {
         
-		_originY = transform.position.y;
 		_navAgent = GetComponent<NavMeshAgent>();
 		_player = GameObject.Find("Player");
 		_attackDetect = transform.GetChild(1).gameObject;
+		_ground = GameObject.Find("Ground");
+		_groundSize = new Vector2(_ground.GetComponent<Renderer>().bounds.size.x, _ground.GetComponent<Renderer>().bounds.size.z);
     }
 
-    void Update()
+    void FixedUpdate()
     {
 		if (!_attack){
-			CheckTarget();
-			if (_target){
-				SetTarget();
+			CheckTarget();;
+			if (!_target){
+				if (!_hasRandomDestination){
+					_hasRandomDestination = true;
+					SetRandomDestination();
+				}
+				else{
+					CheckRandomDestination();
+				}
+			}
+			else{
 				CheckTargetOnRange();
 			}
 		}
     }
+
+	void OnTriggerStay(Collider other){
+		if (other.gameObject.layer == LayerMask.NameToLayer("Player") && _canAttack){
+			print("BAM COUP DE POING DANS LE PLAYER");
+			_canAttack = false;
+		}
+	}
 	#endregion
 	
 	#region Custom Methods
-	private void SetTarget(){
-		_navAgent.destination = _target.transform.position;
-		transform.position = new Vector3(transform.position.x, _originY, transform.position.z);
-	}
-
 	private void CheckTarget(){
 		float distance = Vector3.Distance(transform.position, _player.transform.position);
 		if (distance <= detectionRange && !_target){
+			_hasRandomDestination = false;
 			_target = _player;
+			StartCoroutine(SetTarget());
 		}
 		else if (distance >= chaseRange && _target){
 			_target = null;
@@ -67,9 +86,35 @@ public class ZombieController : MonoBehaviour
 		}
 	}
 
+	private void SetRandomDestination(){
+		Vector3 randomDestination = Vector3.zero;
+		int groundLimit = (int)_groundSize.x / 2;
+		int randomX = Random.Range(-groundLimit + offsetRandomDestination, groundLimit - offsetRandomDestination);
+		int randomZ = Random.Range(-groundLimit + offsetRandomDestination, groundLimit - offsetRandomDestination);
+		randomDestination = new Vector3(randomX, 0, randomZ);
+		_navAgent.destination = randomDestination;
+		print(randomDestination);
+	}
+
+	private void CheckRandomDestination(){
+		if (_navAgent.remainingDistance <= 1){
+			_hasRandomDestination = false;
+		}
+	}
+
 	IEnumerator Attack(){
-		print("Attack");
-		yield return null; //A faire avec les anims
+		_attackDetect.SetActive(true);
+		_canAttack = true;
+		yield return new WaitForSeconds(timeBetweenAttacks);
+		_attack = false;
+		_canAttack = false;
+		_attackDetect.SetActive(false);
+	}
+
+	IEnumerator SetTarget(){
+		_navAgent.destination = transform.position;
+		yield return new WaitForSeconds(1f);
+		_navAgent.destination = _target.transform.position;
 	}
 	#endregion
 }
